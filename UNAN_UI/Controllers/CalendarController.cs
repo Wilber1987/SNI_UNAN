77620a40e0674e184a74921390105755d80a917b
wilber jose matus <wilberj1987@gmail.com>
2022-09-05 08:45:40 -0600

    profiles-ext/panel-profiles#101 ajustes de xhtml para incidentes  nuevo diseño

--- UNAN_UI/Controllers/CalendarController.cs
+++ UNAN_UI/Controllers/CalendarController.cs
@@ -19,36 +19,36 @@ namespace UNAN_UI.Controllers
     public class CalendarController : ControllerBase
     {
         [HttpPost]
-        public Object TakeData()
-        {
-            List<object> data = new List<object>();
-            CatDependencias Dep = new CatDependencias();
-            data.Add(Dep.Get());
-            TblUsuario Users = new TblUsuario();
-            data.Add(Users.Get());
-            Users.IdUsuario = AuthNetCore.User().UserId;
-            data.Add(Users.TakeDepCoordinaciones());
-            return data;
-        }
-        public Object TakeCalendar(Calendar cal)
-        {
-            cal.TakeActividades();
-            cal.TakeAgenda();
-            cal.TakeCalendario();
-            return cal;
-        }
-        public Object TakeActividades(TblActividades Inst)
-        {            
-            return Inst.Get();
-        }
+        //public Object TakeData()
+        //{
+        //    List<object> data = new List<object>();
+        //    CatDependencias Dep = new CatDependencias();
+        //    data.Add(Dep.Get());
+        //    TblUsuario Users = new TblUsuario();
+        //    data.Add(Users.Get());
+        //    Users.IdUsuario = AuthNetCore.User().UserId;
+        //    data.Add(Users.TakeDepCoordinaciones());
+        //    return data;
+        ////}
+        //public Object TakeCalendar(Calendar cal)
+        //{
+        //    cal.TakeActividades();
+        //    cal.TakeAgenda();
+        //    cal.TakeCalendario();
+        //    return cal;
+        //}
+        //public Object TakeActividades(TblActividades Inst)
+        //{            
+        //    return Inst.Get();
+        //}
         public Object TakeActividad(TblActividades Inst)
         {
             return Inst.GetActividad();
         }
-        public Object SaveActividad(TblActividades Act)
-        {            
-            return Act.SaveActividades();
-        }
+        //public Object SaveActividad(TblActividades Act)
+        //{            
+        //    return Act.SaveActividades();
+        //}
         public Object SaveTarea(TblTareas Act)
         {
             return Act.SaveTarea();
@@ -58,13 +58,13 @@ namespace UNAN_UI.Controllers
             return Act.SolicitarActividades();
         }
         //Agenda por usuario
-        public Object AgendaUsuarioDependencia(CatDependencias Act)
-        {
-            TblAgenda ag = new TblAgenda();
-            ag.IdDependencia = Act.IdDependencia;
-            ag.IdUsuario = AuthNetCore.User().UserId;
-            return ag.Get();
-        }
+        //public Object AgendaUsuarioDependencia(CatDependencias Act)
+        //{
+        //    TblAgenda ag = new TblAgenda();
+        //    ag.IdDependencia = Act.IdDependencia;
+        //    ag.IdUsuario = AuthNetCore.User().UserId;
+        //    return ag.Get();
+        //}
         public Object SaveAgendaUsuarioDependencia(TblAgenda Act)
         {
             if (Act.IdAgenda != null)
@@ -82,22 +82,22 @@ namespace UNAN_UI.Controllers
         public List<Object> Agenda { get; set; }
         public List<Object> Calendario { get; set; }
 
-        public void TakeAgenda()
-        {
-            TblAgenda ag = new TblAgenda();
-            ag.IdDependencia = this.IdDependencia;
-            this.Agenda = ag.Get();
-        }
-        public void TakeActividades()
-        {
-            TblActividades ag = new TblActividades();
-            this.Actividades = ag.Get();
-        }
-        public void TakeCalendario()
-        {
-            ViewCalendarioByDependencia ag = new ViewCalendarioByDependencia();
-            ag.IdDependencia = this.IdDependencia;
-            this.Calendario = ag.Get();
-        }
+        //public void TakeAgenda()
+        //{
+        //    TblAgenda ag = new TblAgenda();
+        //    ag.IdDependencia = this.IdDependencia;
+        //    this.Agenda = ag.Get();
+        //}
+        //public void TakeActividades()
+        //{
+        //    TblActividades ag = new TblActividades();
+        //    this.Actividades = ag.Get();
+        //}
+        //public void TakeCalendario()
+        //{
+        //    ViewCalendarioByDependencia ag = new ViewCalendarioByDependencia();
+        //    ag.IdDependencia = this.IdDependencia;
+        //    this.Calendario = ag.Get();
+        //}
     }
 }
